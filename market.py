653c1460f8af4619e9a903eb75007236803ba109
Dimitry <tizya.noonie@gmail.com>
2022-12-02 13:18:38 +0300

    Initial commit

---
+++ market.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return '<h1>Hello World!!!</h1>'
+
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1>This is the about page of {username}</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
